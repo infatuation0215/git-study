25b7ed44b9da38cfc5137bf885b87f25bf45d586
steven <471679474@qq.com>
2023-01-08 13:50:07 +0800

    forth commit

--- src/main/java/GitTest.java
+++ src/main/java/GitTest.java
@@ -3,5 +3,6 @@ public class GitTest {
         System.out.println("hello git first");
         System.out.println("hello git second");
         System.out.println("hello git third");
+        System.out.println("hello git forth");
     }
 }
